cdf5d4f76cdb408a337ea25f941e22d4755d9fa1
flashlin <flash.lin@gmail.com>
2021-06-28 20:19:07 +0800

    add basic sample

---
+++ 001-Basic/BlazorSample2/Models/IMyInterface.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorSample2.Models
+{
+	public interface IMyInterface
+	{
+		string GetHello();
+	}
+
+	public class MyService : IMyInterface
+	{
+		public string GetHello()
+		{
+			return "Hello, Flash";
+		}
+	}
+}
---
+++ 001-Basic/BlazorSample2/Models/MySelectOption.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorSample2.Models
+{
+	public class MySelectOption
+	{
+		public string Text { get; set; }
+		public int Value { get; set; }
+	}
+}
