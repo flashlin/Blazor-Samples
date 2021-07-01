291c3427deb930856afb6bb9a5acbae3e6d01f01
flashlin <flash.lin@gmail.com>
2021-07-01 19:58:56 +0800

    add CodeBehind Sample

---
+++ 004-CodeBehind/CodeBehindSample/Pages/Counter.razor.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+
+namespace CodeBehindSample.Pages
+{
+	public partial class Counter : ComponentBase
+	{
+		private int currentCount = 0;
+
+		private void IncrementCount()
+		{
+			currentCount++;
+		}
+
+	}
+}
---
+++ 004-CodeBehind/CodeBehindSample/Pages/JsonBeautify.razor.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+
+namespace CodeBehindSample.Pages
+{
+	public partial class JsonBeautify : ComponentBase
+	{
+		private string _jsonData;
+		private string _formattedJsonData;
+
+		private void ConvertJsonDataToFormattedJsonData()
+		{
+			var obj = JsonDocument.Parse(_jsonData);
+			using (var stream = new MemoryStream())
+			{
+				var writer = new Utf8JsonWriter(stream, new JsonWriterOptions()
+				{
+					Indented = true
+				});
+				obj.WriteTo(writer);
+				writer.Flush();
+				_formattedJsonData = Encoding.UTF8.GetString(stream.ToArray());
+			}
+		}
+	}
+}
