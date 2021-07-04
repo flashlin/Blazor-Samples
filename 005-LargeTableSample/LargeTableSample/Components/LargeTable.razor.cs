0e4245468a15cd8dd6b71874fbec29e80f608d24
flashlin <flash.lin@gmail.com>
2021-07-04 09:26:50 +0800

    add larget project

---
+++ 005-LargeTableSample/LargeTableSample/Components/LargeTable.razor.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+
+namespace LargeTableSample.Components
+{
+	public partial class LargeTable : ComponentBase
+	{
+		ElementReference BodyReference;
+
+		[Inject] 
+		private IJSRuntime Js { get; set; }
+
+		private async Task HandleOnScroll(EventArgs e)
+		{
+			var scrollInfo = await Js.InvokeAsync<ScrollInfo>("BlazorUniversity.getScrollInfo", BodyReference);
+			var scrollValue = scrollInfo.ScrollTop + scrollInfo.ClientHeight;
+			Console.WriteLine($"scrollValue={scrollValue} scHeight={scrollInfo.ScrollHeight}");
+		}
+	}
+
+	public class ScrollInfo
+	{
+		public int ScrollHeight { get; set; }
+		public int ScrollTop { get; set; }
+		public int ClientHeight { get; set; }
+	}
+}
---
+++ 005-LargeTableSample/LargeTableSample/wwwroot/javascripts/BlazorUniversity.js
@@ -0,0 +1,8 @@
+﻿var BlazorUniversity = BlazorUniversity || {};
+BlazorUniversity.getScrollInfo = function (element) {
+	return {
+		ScrollHeight: element.scrollHeight,
+		ScrollTop: element.scrollTop,
+		ClientHeight: element.clientHeight
+	};
+};
